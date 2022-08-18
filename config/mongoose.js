bd67bb625e3209db6e0d5be9ff2350909b92333a
shubham123456789012 <shubham_ug@ece.nits.ac.in>
2022-08-19 03:57:43 +0530

    deploy to heroku

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://Shubham:shubhamkumar@cluster0.fwbpo.mongodb.net/rental?retryWrites=true&w=majority');
 const db=mongoose.connection;
 //error
-db.on('error',console.error.bind(console,'error while connecting'));
+db.on('error',console.error.bind(console,'error while making connection'));
 //up and running then print the message.
 db.once('open',()=>{
     console.log('connection successfully');
